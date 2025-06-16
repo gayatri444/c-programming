1521faa308f9e41d650423c65f582a5b6c58275d
gayatri444 <gayatriraykar09@gmail.com>
2025-06-16 10:19:31 +0530

    4th commit

--- minimumNumbers2.c
+++ minimumNumbers2.c
@@ -4,9 +4,9 @@ int main()
     int a,b;
     printf("enter 2 number");
     scanf("%d%d",&a,&b);
-    if(a>b)
+    if(a<b)
     printf("minimum is %d",a,b);
 else
-printf("minimum is %d",(a>b)?a:b);
+printf("minimum is %d",(a<b)?a:b);
 return 0;
 }
